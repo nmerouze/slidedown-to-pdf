46be8bee622e4c1d2648a3490b6ac247796a7a08
Michael Bernstein <simoniac@gmail.com>
2009-12-05 12:40:04 +0800

    Made some modifications to make this script a bit more generic - aka, simple module encapsulation, more command line options for height/width, etc.  Useful for designing pdfs from html and having them generated on demand

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ If you use [slidedown](http://github.com/nakajima/slidedown) and you want your s
 macruby to_pdf.rb -n 27 http://nmerouze.github.com/blockcampparis09 myfilename.pdf</code></pre>
 
 **NOTE**: If your last slide is #26, you have 27 slides (because there's a slide #0).
+
+
+Unsure of what license nmerouze wanted to release this as..
+but afaik, it's (C) 2009 Nicolas Mérouze
+with parts (C) 2009 Michael Bernstein
+
+I'll release my minor changes under whatever license he meant to, so he can edit this if he takes my patch :P.
---
+++ to_pdf.rb
@@ -0,0 +1,84 @@
+
+
+module ToPDF
+  class VirtualBrowser
+    attr_reader :view, :file_name
+
+    def initialize(height, width)
+      NSApplication.sharedApplication.delegate = self
+      @view     = WebView.alloc.initWithFrame([0, 0, width, height])
+      window    = NSWindow.alloc.initWithContentRect([0, 0, width, height],
+                                                  styleMask:NSBorderlessWindowMask, 
+                                                  backing:NSBackingStoreBuffered, 
+                                                  defer:false)
+
+      window.contentView = view
+      setup_view_prefs
+      view.frameLoadDelegate = self
+      @captured = false
+      @doc = PDFDocument.alloc.init
+    end
+
+    def fetch(url, number, file_name)
+      0.upto(number - 1) do |i|
+        page_url = NSURL.URLWithString("#{url}/##{i}")
+        view.mainFrame.loadRequest NSURLRequest.requestWithURL(page_url)
+        view.mainFrame.reload
+        until @captured
+          NSRunLoop.currentRunLoop.runUntilDate NSDate.date
+        end
+        @captured = false
+      end
+  
+      filepath = File.expand_path(file_name)
+      puts "saving #{filepath}"
+      @doc.writeToFile(filepath, atomically:true)
+    end
+
+    def setup_view_prefs
+      view.mediaStyle = 'screen'
+      view.customUserAgent = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_2; en-us) AppleWebKit/531.21.8 (KHTML, like Gecko) Version/4.0.4 Safari/531.21.10'
+      view.preferences.autosaves = false
+      view.preferences.shouldPrintBackgrounds = true
+      view.preferences.javaScriptCanOpenWindowsAutomatically = false
+      view.preferences.allowsAnimatedImages = false
+      view.mainFrame.frameView.allowsScrolling = false
+    end
+
+    def webView(view, didFinishLoadForFrame:frame)
+      NSRunLoop.currentRunLoop.runUntilDate NSDate.dateWithTimeIntervalSinceNow(0.2)
+      save
+    end
+
+    def webView(view, didFailLoadWithError:error, forFrame:frame)
+      raise "Failed to take snapshot: #{error.localizedDescription}"
+      quit
+    end
+
+    def webView(view, didFailProvisionalLoadWithError:error, forFrame:frame)
+      raise "Failed to take snapshot: #{error.localizedDescription}"
+      quit
+    end
+
+    def quit
+      NSApplication.sharedApplication.terminate(nil)
+    end
+
+    def save
+      doc_view = setup_docview
+      doc = PDFDocument.alloc.initWithData(doc_view.dataWithPDFInsideRect(doc_view.bounds))
+      doc_view.unlockFocus
+      @doc.insertPage(doc.pageAtIndex(0), atIndex:@doc.pageCount)
+      @captured = true
+    end
+
+    def setup_docview
+      doc_view = view.mainFrame.frameView.documentView
+      doc_view.window.contentSize = [doc_view.bounds.size.width, doc_view.bounds.size.height]
+      doc_view.frame = view.bounds
+      doc_view.needsDisplay = true
+      doc_view.lockFocus
+      doc_view
+    end
+  end
+end
